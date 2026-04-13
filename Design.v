d86065a418b45d4ff3ec527c0940cc8bfe796bbe
Akulkute <kuteakul996@gmail.com>
2026-04-13 21:12:41 +0530

    Create baseline_mac module for signed multiplication
    Added a module for baseline MAC with signed inputs and outputs.

--- Design.v
+++ Design.v
@@ -1,4 +1,23 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/09/2026 08:24:07 PM
+// Design Name: 
+// Module Name: baseline_mac
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 module baseline_mac (
     input  signed [7:0] a,
@@ -6,6 +25,7 @@ module baseline_mac (
     output signed [15:0] p
 );
 
+    // Xilinx-specific attribute to force LUT-based synthesis instead of DSP slices
     (* use_dsp = "no" *) 
     wire signed [15:0] mult_out;
 
